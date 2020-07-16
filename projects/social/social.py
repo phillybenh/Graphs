152decbe672281090f4ab4f8ef5f6ecdc85559a1
Ben Haus <ben@little-haus.com>
2020-07-16 15:33:23 -0400

    initial commit for sprint-challenge

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,4 +1,6 @@
 import random
+import time
+# from collections import deque TODO: try implementing with this
 from util import Stack, Queue
 
 class User:
@@ -7,6 +9,9 @@ class User:
 
 class SocialGraph:
     def __init__(self):
+        self.reset()
+
+    def reset(self):
         self.last_id = 0
         self.users = {}
         self.friendships = {}
@@ -16,12 +21,15 @@ class SocialGraph:
         Creates a bi-directional friendship
         """
         if user_id == friend_id:
-            print("WARNING: You cannot be friends with yourself")
+            # print("WARNING: You cannot be friends with yourself")
+            return False
         elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
-            print("WARNING: Friendship already exists")
+            # print("WARNING: Friendship already exists")
+            return False
         else:
             self.friendships[user_id].add(friend_id)
             self.friendships[friend_id].add(user_id)
+            return True
 
     def add_user(self, name):
         """
@@ -42,9 +50,10 @@ class SocialGraph:
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        # self.last_id = 0
+        # self.users = {}
+        # self.friendships = {}
+        self.reset()
 
         # Add users
         for i in range(num_users):
@@ -64,6 +73,29 @@ class SocialGraph:
         for i in range(num_users * avg_friendships // 2):
             friendship = possible_friendships[i]
             self.add_friendship(friendship[0], friendship[1])
+
+    def populate_graph_2(self, num_users, avg_friendships):
+        # Reset graph
+        self.reset()
+        collisions = 0
+
+        # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i+1}")
+
+        # Create friendships
+        target_friendships = num_users * avg_friendships
+        total_friendships = 0
+
+        while total_friendships < target_friendships:
+            user_id = random.randint(1, self.last_id)
+            friend_id = random.randint(1, self.last_id)
+
+            if self.add_friendship(user_id, friend_id):
+                total_friendships += 2
+            else:
+                collisions += 1
+        print(f"COLLISIONS: {collisions}")
 
 
     def get_all_social_paths(self, user_id):
@@ -98,7 +130,21 @@ class SocialGraph:
 
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
-    print(sg.friendships)
+
+    num_users = 10
+    avg_friendships = 2
+    start_time1 = time.time()
+    sg.populate_graph(num_users, avg_friendships)
+    end_time1 = time.time()
+    print(f"O(n^2) runtime: {end_time1 - start_time1}")
+    start_time2 = time.time()
+    sg.populate_graph_2(num_users, avg_friendships)
+    end_time2 = time.time()
+    print(f"O(n) runtime: {end_time2 - start_time2}")
+    # An n increases, our O(n) version is not always better than the 
+    # O(n^2) due to the number of collisions that occur 
+
+    print("Friendships: ", sg.friendships)
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print("Connections: ", connections)
+    
